e1b8de057a810604abe52d1cca10d45605e5c712
tatha007 <tatha.nandy51@gmail.com>
2025-02-28 21:56:15 -0800

    updated App.java to change the URL of the Insure-me app to my Test serverIP

--- src/main/java/com/selenium/test/insure_me_test/App.java
+++ src/main/java/com/selenium/test/insure_me_test/App.java
@@ -13,41 +13,41 @@ import dev.failsafe.internal.util.Assert;
  * Hello world!
  */
 public class App {
-	
-	
+
+
     public static void main(String[] args) {
-       
-    	
+
+
     	ChromeOptions options = new ChromeOptions();
    	 	options.addArguments("--headless");
-    	 
+
    	 	WebDriver driver = new ChromeDriver(options);
-    	 
-    	
-         driver.get("http://3.145.61.169:8081/contact.html");
+
+
+         driver.get("http://35.94.126.133:8081/contact.html");
 
          System.out.println(driver.getTitle());
 
          WebElement name = driver.findElement(By.id("inputName"));
          name.sendKeys("First Name");
-         
+
          WebElement number = driver.findElement(By.id("inputNumber"));
          number.sendKeys("999999999");
-         
+
          WebElement email = driver.findElement(By.id("inputMail"));
          email.sendKeys("shubham@xyz.com");
-         
+
          WebElement message = driver.findElement(By.id("inputMessage"));
          message.sendKeys("Hello, How are you ");
-         
+
          WebElement submitButton = driver.findElement(By.id("my-button"));
          submitButton.click();
 
          WebElement response = driver.findElement(By.id("response"));
          response.getText();
-         
+
          System.out.println(response.getText());
-         
+
          if(response.getText().equalsIgnoreCase("Message Sent")) {
         	 System.out.println("Test Case Passed");
          }
@@ -55,13 +55,13 @@ public class App {
          {
         	 System.out.println("Text Case Failed");
          }
-        
+
 
          driver.quit();
-    	
-    	
-    	
-    	
-    	
+
+
+
+
+
     }
 }
